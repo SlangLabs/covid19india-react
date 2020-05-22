e8d302fd357de96c9655cefc693d518b208ba927
nisdis <niszim@gmail.com>
2020-05-22 14:11:23 +0000

    Deploying to gh-pages from  @ 71029505ce2e968e33893af65aeafd06f43122e8 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f0171b4dba8d9a4515adbee5229e322c.js"
+  "/precache-manifest.8a235a3502e801de48efa49e9e487d98.js"
 );
 
 self.addEventListener('message', (event) => {
